5f9d5285f797c7a66a3d9e2d48832eebf360bcca
mughees-raza <mughees.raza@arbisoft.com>
2025-01-28 14:53:08 +0500

    feat: write command output to specified file instead of console

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::env;
+use std::{env, fs::File, io::Write};
 
 mod args_parser;
 mod commands;
@@ -7,19 +7,16 @@ mod parser;
 fn main() {
     let args = args_parser::Arguments::build(env::args()).unwrap();
     let mut parser = parser::Parser::new(&args.input_file_path.as_path()).unwrap();
-    let mut code = vec![];
+    let mut output_file = File::create(args.output_file_path).unwrap();
 
     while parser.has_more_command() {
         let (c, command) = parser.advance();
-
         let results = command.generate();
 
-        println!("// {0}", c);
+        writeln!(output_file, "// {}", c).unwrap();
 
         for i in &results {
-            println!("{i}");
+            writeln!(output_file, "{}", i).unwrap();
         }
-
-        code.extend(results);
     }
 }
